ad23719bdabb108c483ab59a2b1752c21f00afff
NiTr088 <nitro@nitro.(none)>
2011-05-16 18:47:20 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '22ea6503427c09c5b132d1155279492d80ffff278b2a5697c6efad8cbe82f1a3ed402ad129a428b151fa9ec1e4715fdcf0c3af1197c036ba05473f0ec7b9d8f3'
